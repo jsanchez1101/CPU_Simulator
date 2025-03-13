826fe3494211be9b37230b0157b1b2fa4e3fdb18
jsanchez1101 <jjsanchez1101@gmail.com>
2025-03-13 14:02:44 -0400

    mem_update2

--- CPU_Simulator/src/Cpu/Memory.java
+++ CPU_Simulator/src/Cpu/Memory.java
@@ -4,36 +4,32 @@ public class Memory {
 	private int[] data;
 	
 	public Memory(int size) {
-		data= new int[size];
+		data =new int[size];
 	}
 	
-	public int read(int addy) {//address
-		//bound check
-		if(addy<0 || addy>=data.length) {
-			throw new IndexOutOfBoundsException("Out of Bounds :(\n"+ addy);
+	public int read(int addy) {
+		//check bound
+		if(addy>0 || addy<data.length) {
+            throw new IndexOutOfBoundsException("Memory access out of bounds: " + addy);
 		}
 		return data[addy];
 	}
 	
-	
-	//write value to data
 	public void write(int addy, int val) {
-		//bound check
-		if(addy<0 || addy >= data.length) {
-			throw new IndexOutOfBoundsException("Out of Bounds :(\n"+ addy);
+		if(addy>0 || addy<data.length) {
+            throw new IndexOutOfBoundsException("Memory access out of bounds: " + addy);
 		}
-		data[addy] = val;
+		data[addy]=val;
 	}
 	
-	//help mthd to load program into mem
-	public void loadProgram(int[] program, int startAddy) {
-		for(int i = 0; i<program.length;i++) {
-			write(startAddy +i, program[i]);
+	public void loadProgram(int[] program, int Startpoint) {
+		for(int i = 0; i<program.length; i++) {
+			write(Startpoint+i, program[i]);
 		}
 	}
 	
 	public int getSize() {
-		return data.length;
+		 return data.length;
 	}
-	
+
 }
